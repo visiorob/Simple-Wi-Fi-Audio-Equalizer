fad33594cb447227161832e7e8c8d14ddd6c7411
marcelo-rossi <gesseemail@gmail.com>
2025-10-18 11:24:37 -0300

    HTML calcula os coeficientes do filtro e envia para a ESP32-S3

--- main/main.cpp
+++ main/main.cpp
@@ -2,11 +2,15 @@
 //                        INCLUSÃO DE BIBLIOTECAS
 // ===================================================================
 #include <Arduino.h>
+#include <ArduinoJson.h>
 #include <WiFi.h>
 #include <ESPAsyncWebServer.h>
 #include <ESPmDNS.h>
 #include "driver/i2s.h"
 #include <math.h>
+#include "esp_task_wdt.h"
+#include <freertos/FreeRTOS.h>
+#include <freertos/queue.h>
 #include "esp_task_wdt.h"
 // ===================================================================
 //                      CONFIGURAÇÕES GLOBAIS
@@ -36,10 +40,14 @@ const int16_t fir_mid[FIR_ORDER] = { -3, -2, 1, 6, 11, 15, 17, 16, 11, 3, -10, -
 const int16_t fir_high_mid[FIR_ORDER] = { -1, 3, 0, -4, 2, 4, -4, -5, 7, 6, -10, -9, 14, 13, -19, -19, 25, 26, -33, -35, 43, 47, -56, -62, 73, 82, -96, -108, 126, 143, -165, -188, 216, 11883, 216, -188, -165, 143, 126, -108, -96, 82, 73, -62, -56, 47, 43, -35, -33, 26, 25, -19, -19, 13, 14, -9, -10, 6, 7, -5, -4, 2, -4, 0, 3 };
 const int16_t fir_high[FIR_ORDER] = { -1, 2, -1, -2, 3, -2, -3, 5, -4, -4, 7, -6, -6, 10, -8, -9, 14, -11, -13, 19, -16, -18, 26, -23, -25, 35, -32, -35, 48, -45, -49, 66, -64, -22064, -64, 66, -49, -45, 48, -35, -32, 35, -25, -23, 26, -18, -16, 19, -13, -11, 14, -9, -8, 10, -6, -6, 7, -4, -4, 5, -3, -2, -1, 2, -1 };
 
+int32_t fir_coeffs[FIR_ORDER];
+int32_t fir_coeffs2[FIR_ORDER];
+QueueHandle_t fir_coeffs_Queue;
+
 // --- Buffers e Variáveis Globais ---
 int16_t history_low[FIR_ORDER] = {0}, history_low_mid[FIR_ORDER] = {0}, history_mid[FIR_ORDER] = {0}, history_high_mid[FIR_ORDER] = {0}, history_high[FIR_ORDER] = {0};
 volatile float gain_low = 1.0, gain_low_mid = 1.0, gain_mid = 1.0, gain_high_mid = 1.0, gain_high = 1.0;
-
+int16_t filter[FIR_ORDER] = {0};
 // --- Servidor Web ---
 AsyncWebServer server(80);
 
@@ -62,9 +70,23 @@ int32_t apply_fir_filter(int16_t sample, const int16_t* coeffs, int16_t* history
     return (int32_t)(result >> 15);
 }
 
+int32_t apply_fir_filter_2(int16_t sample, int16_t* history)
+{
+    if (uxQueueMessagesWaiting(fir_coeffs_Queue) > 0) 
+    {
+        xQueueReceive(fir_coeffs_Queue, fir_coeffs2, portMAX_DELAY);
+    }
+    for (int i = FIR_ORDER - 1; i > 0; i--) { history[i] = history[i - 1]; }
+    history[0] = sample;
+    int64_t result = 0;
+    for (int i = 0; i < FIR_ORDER; i++) { result += (int64_t)history[i] * fir_coeffs2[i]; }
+    return (int32_t)(result >> 15);
+
+}
+
 // --- Tarefa de Áudio e Equalização ---
 void audio_task(void* param) {
-    esp_task_wdt_delete(NULL);
+    //esp_task_wdt_delete(NULL);
 
     // Tamanho do arquivo WAV
     musica_len = musica_wav_end - musica_wav_start; 
@@ -91,16 +113,19 @@ void audio_task(void* param) {
     size_t buf_index = 0;
 
     while (true) {
+        // esp_task_wdt_reset(); // reinicia o contador do watchdog
+
         int16_t original_sample = samples[i++];
-        int32_t low_out      = apply_fir_filter(original_sample, fir_low, history_low);
-        int32_t low_mid_out  = apply_fir_filter(original_sample, fir_low_mid, history_low_mid);
+        int32_t low_out      = apply_fir_filter_2(original_sample, history_low);
+        // int32_t low_out      = apply_fir_filter(original_sample, fir_low, history_low);
+        // int32_t low_mid_out  = apply_fir_filter(original_sample, fir_low_mid, history_low_mid);
         // int32_t mid_out      = apply_fir_filter(original_sample, fir_mid, history_mid);
         // int32_t high_mid_out = apply_fir_filter(original_sample, fir_high_mid, history_high_mid);
         // int32_t high_out     = apply_fir_filter(original_sample, fir_high, history_high);
         
         // mixagem multiplicativa dos ganhos com os áudios filtrados
         int64_t final_sum = (int64_t)(low_out * gain_low)
-                          + (int64_t)(low_mid_out * gain_low_mid)
+                        //   + (int64_t)(low_mid_out * gain_low_mid)
                         //   + (int64_t)(mid_out * gain_mid)
                         //   + (int64_t)(high_mid_out * gain_high_mid)
                         //   + (int64_t)(high_out * gain_high)
@@ -134,6 +159,7 @@ void audio_task(void* param) {
 
 // --- Setup ---
 void setup() {
+    fir_coeffs_Queue = xQueueCreate(1, sizeof(fir_coeffs));
     controle_len = controle_html_end - controle_html_start;
     Serial.begin(115200);
     delay(1000);
@@ -156,20 +182,62 @@ void setup() {
         Serial.println("Servidor mDNS iniciado. Acesse em http://equalizer.local");
     }
     server.on("/", HTTP_GET, [](AsyncWebServerRequest *r){ r->send_P(200, "text/html", controle_html_start, controle_len); });
-    server.on("/update", HTTP_GET, [] (AsyncWebServerRequest *r) {
-        if (r->hasParam("band") && r->hasParam("gain")) {
-            String b = r->getParam("band")->value();
-            float g = r->getParam("gain")->value().toFloat();
-            if (b == "low") gain_low = g;
-            else if (b == "low_mid") gain_low_mid = g;
-            else if (b == "mid") gain_mid = g;
-            else if (b == "high_mid") gain_high_mid = g;
-            else if (b == "high") gain_high = g;
-            r->send(200, "text/plain", "OK");
-        } else {
-            r->send(400, "text/plain", "Faltam parametros");
+    // server.on("/update", HTTP_GET, [] (AsyncWebServerRequest *r) {
+    //     if (r->hasParam("band") && r->hasParam("gain")) {
+    //         String b = r->getParam("band")->value();
+    //         float g = r->getParam("gain")->value().toFloat();
+    //         if (b == "low") gain_low = g;
+    //         else if (b == "low_mid") gain_low_mid = g;
+    //         else if (b == "mid") gain_mid = g;
+    //         else if (b == "high_mid") gain_high_mid = g;
+    //         else if (b == "high") gain_high = g;
+    //         // else if (b == "filter") filter = g;
+    //         r->send(200, "text/plain", "OK");
+    //     } else {
+    //         r->send(400, "text/plain", "Faltam parametros");
+    //     }
+    // });
+    server.on("/update", HTTP_POST, [](AsyncWebServerRequest *request){},
+        NULL,  // não usamos upload handler
+        [](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total) {
+        // data contém o corpo do POST
+        StaticJsonDocument<2048> doc; // tamanho suficiente para 65 inteiros
+        DeserializationError error = deserializeJson(doc, data, len);
+
+        if (error) {
+            Serial.print("Erro ao parsear JSON: ");
+            Serial.println(error.c_str());
+            request->send(400, "text/plain", "JSON inválido");
+            return;
         }
-    });
+
+        if (!doc.containsKey("coeffs")) {
+            request->send(400, "text/plain", "Faltam coeficientes");
+            return;
+        }
+
+        JsonArray arr = doc["coeffs"].as<JsonArray>();
+        if (arr.size() != 65) {
+            request->send(400, "text/plain", "Esperados 65 coeficientes");
+            return;
+        }
+
+        for (int i = 0; i < 65; i++) {
+            fir_coeffs[i] = arr[i].as<int16_t>();
+        }
+
+        xQueueOverwrite(fir_coeffs_Queue, fir_coeffs);
+
+        // Serial.println("Coeficientes recebidos:");
+        // for (int i = 0; i < 65; i++) {
+        //     Serial.print(fir_coeffs[i]);
+        //     Serial.print(i < 64 ? ", " : "\n");
+        // }
+
+        request->send(200, "text/plain", "OK");
+        }
+    );
+
     server.begin();
     Serial.println("Servidor Web iniciado.");
     i2s_config_t i2s_config = { .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX), .sample_rate = SAMPLE_RATE, .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT, .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT, .communication_format = I2S_COMM_FORMAT_STAND_I2S, .intr_alloc_flags = 0, .dma_buf_count = 8, .dma_buf_len = BUFFER_SIZE, .use_apll = false };
@@ -179,7 +247,7 @@ void setup() {
     i2s_zero_dma_buffer(I2S_NUM_0);
    
     // aumentei a prioridade da tarefa de audio pra reduzir ruido do wi-fi
-    esp_task_wdt_delete(xTaskGetIdleTaskHandleForCore(1));
+    //esp_task_wdt_delete(xTaskGetIdleTaskHandleForCore(1));
     xTaskCreatePinnedToCore(audio_task, "EqualizerTask", 8192, NULL, configMAX_PRIORITIES - 1, NULL, 1);
 
     Serial.println("Setup concluido. Equalizador 5 Bandas funcional.");
